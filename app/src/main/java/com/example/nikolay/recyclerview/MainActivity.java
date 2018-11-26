af9d65cf87a67160aacac9adc2a8b68888438741
kx30 <nikolay199808@gmail.com>
2018-11-26 23:35:52 +0300

    Added connection check

--- app/src/main/java/com/example/nikolay/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/nikolay/recyclerview/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.nikolay.recyclerview;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.nikolay.recyclerview.fragments.NewPicturesFragment;
 import com.example.nikolay.recyclerview.fragments.PopularPicturesFragment;
@@ -33,10 +37,31 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setAdapter(adapter);
         mTabLayout.setupWithViewPager(mViewPager);
 
-
+        if (!hasConnection(this)) {
+            Log.d(TAG, "onCreate: No connection!");
+        } else {
+            Log.d(TAG, "onCreate: Connection!");
+        }
 
         Log.d(TAG, "onCreate: started");
     }
 
+
+    public static boolean hasConnection(final Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        wifiInfo = cm.getActiveNetworkInfo();
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
 
 }
